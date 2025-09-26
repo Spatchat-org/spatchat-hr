248846f18bb90abd13890e538eafacd87b5910fe
Ho Yi Wan <hoyiwan@users.noreply.huggingface.co>
2025-09-26 18:45:43 +0000

    Create map_utils.py

---
+++ map_utils.py
@@ -0,0 +1,24 @@
+import folium
+import numpy as np
+
+def render_empty_map():
+    m = folium.Map(location=[0, 0], zoom_start=2, control_scale=True)
+    folium.TileLayer("OpenStreetMap").add_to(m)
+    folium.TileLayer("CartoDB positron", attr='CartoDB').add_to(m)
+    folium.TileLayer(
+        tiles="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+        attr="OpenTopoMap", name="Topographic"
+    ).add_to(m)
+    folium.TileLayer(
+        tiles="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+        attr="Esri", name="Satellite"
+    ).add_to(m)
+    folium.LayerControl(collapsed=False).add_to(m)
+    return m._repr_html_()
+
+def fit_map_to_bounds(m, df):
+    min_lat, max_lat = df['latitude'].min(), df['latitude'].max()
+    min_lon, max_lon = df['longitude'].min(), df['longitude'].max()
+    if np.isfinite([min_lat, max_lat, min_lon, max_lon]).all():
+        m.fit_bounds([[min_lat, min_lon], [max_lat, max_lon]])
+    return m
